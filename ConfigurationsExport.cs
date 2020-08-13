8c76f21372a071ab4c8416c12d869e78d5acbafd
nanibabu-sumt <69617659+nanibabu-sumt@users.noreply.github.com>
2020-08-13 10:35:56 +0530

    Add files via upload
    Initial Upload

---
+++ ConfigurationsExport.cs
@@ -0,0 +1,66 @@
+﻿using Sumtotal.ConfigurationsAutomation.Contracts;
+using SumTotal.Framework.Core.Contracts.Logging;
+using SumTotal.Framework.Core.MultiTenancy;
+using SumTotal.Services.Jobs.Contracts;
+using SumTotal.Services.Jobs.JobProcessors;
+using System;
+using System.Collections.Generic;
+
+namespace Sumtotal.ConfigurationsAutomation
+{
+    public class ConfigurationsExport : BaseCustomJob
+    {
+        ServiceJobContext jobContext = new ServiceJobContext();
+
+        bool disposed = false;
+        IBaseExtract baseExtract;
+        public override void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public override void Execute(ServiceJobContext context, IDictionary<string, object> parameters)
+        {
+            string group;
+            group = parameters["Group"].ToString();
+            group = "II";
+            baseExtract = SettingsFactory.GetExtractor(group);
+            baseExtract.LoadDependencies(_logger);
+            baseExtract.Execute(context, parameters);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposed)
+                    return;
+
+                if (disposing)
+                {
+                    if (jobContext != null)
+                    {
+                        //Call IDisposable on any that implements the interface
+                        foreach (KeyValuePair<string, object> kvp in jobContext.JobParameters)
+                        {
+                            IDisposable iDisposable = kvp.Value as IDisposable;
+                            if (iDisposable != null)
+                                iDisposable.Dispose();
+                        }
+                    }
+
+                    jobContext = null;
+                }
+            }
+            finally
+            {
+                disposed = true;
+            }
+        }
+        public ConfigurationsExport(ITenant tenant, ILogger logger) : base(tenant, logger)
+        {
+
+        }
+    }
+}
---
+++ SettingsFactory.cs
@@ -0,0 +1,21 @@
+﻿using Sumtotal.ConfigurationsAutomation.Contracts;
+using Sumtotal.ConfigurationsAutomation.Services;
+
+namespace Sumtotal.ConfigurationsAutomation
+{
+    public static class SettingsFactory
+    {
+        public static IBaseExtract GetExtractor(string phase)
+        {
+            switch (phase)
+            {
+                case "I": return new ExtractPhase_I();
+                case "II": return new ExtractPhase_II();
+                case "III": return new ExtractPhase_III();
+                case "IV": return new Export();
+                case "V": return new Import();
+                default: return new BaseExtract();
+            }
+        }
+    }
+}
